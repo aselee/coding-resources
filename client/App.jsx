bb79bc637e6d398bd9d3618cc23757a9c08e5b07
Andrew <32229265+aselee@users.noreply.github.com>
2018-11-13 18:44:01 -0600

    deleting

--- client/App.jsx
+++ client/App.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import { Header, Footer, SideBar } from './components';
+import Header from './components/Header';
+// import { Header, Footer, SideBar } from './components';
 import './App.scss';
 
-export class AppSite extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="App">
-          <SideBar />
-          <Header />
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+// export class AppSite extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div className="App">
+//           {/* <SideBar /> */}
+//           <Header />
+//           {/* <Footer /> */}
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-export default AppSite;
+// export default AppSite;
 
-// const app = () => (
-//   <div className="app">
-//     <SideBar />
-//     <Header />
-//     <Footer />
-//   </div>
-// );
+const app = () => (
+  <div className="app">
+    <SideBar />
+    <Header />
+    <Footer />
+  </div>
+);
 
-// export default app;
+export default app;
--- client/components/SideBar/SideBar.jsx
+++ client/components/SideBar/SideBar.jsx
@@ -1,85 +1,85 @@
-import React, { Component } from 'react';
-import {
-  AppBar,
-  ToolBar,
-  IconButton, 
-  Typography, 
-  Hidden,
-  Drawer
-} from '@material-ui/core';
+// import React, { Component } from 'react';
+// import {
+//   AppBar,
+//   ToolBar,
+//   IconButton, 
+//   Typography, 
+//   Hidden,
+//   Drawer
+// } from '@material-ui/core';
 
-export default class extends Component {
-  state = {
-    mobileOpen: false
-  }
+// export default class extends Component {
+//   state = {
+//     mobileOpen: false
+//   }
 
-  handleDrawerToggle = () => {
-    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-  };
+//   handleDrawerToggle = () => {
+//     this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+//   };
 
-  render() {
-    const {classes} = this.props
-    const {mobileOpen} = this. state
+//   render() {
+//     const {classes} = this.props
+//     const {mobileOpen} = this. state
 
-    const drawer = (
-      <div>
-        <div className={classes.toolbar} />
-        testtesttets
-        <Divider />
-      </div>
-    );
+//     const drawer = (
+//       <div>
+//         <div className={classes.toolbar} />
+//         testtesttets
+//         <Divider />
+//       </div>
+//     );
 
-    return (
-      <div className={classes.root}>
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.navIconHide}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Responsive drawer
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Hidden mdUp>
-          <Drawer
-            variant="temporary"
-            open={mobileOpen}
-            onClose={this.handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden smDown implementation="css">
-          <Drawer
-            variant="permanent"
-            open
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
-        </main>
-      </div>
-    )
-  }
-}
+//     return (
+//       <div className={classes.root}>
+//         <AppBar className={classes.appBar}>
+//           <Toolbar>
+//             <IconButton
+//               color="inherit"
+//               aria-label="Open drawer"
+//               onClick={this.handleDrawerToggle}
+//               className={classes.navIconHide}
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Typography variant="h6" color="inherit" noWrap>
+//               Responsive drawer
+//             </Typography>
+//           </Toolbar>
+//         </AppBar>
+//         <Hidden mdUp>
+//           <Drawer
+//             variant="temporary"
+//             open={mobileOpen}
+//             onClose={this.handleDrawerToggle}
+//             classes={{
+//               paper: classes.drawerPaper,
+//             }}
+//             ModalProps={{
+//               keepMounted: true, // Better open performance on mobile.
+//             }}
+//           >
+//             {drawer}
+//           </Drawer>
+//         </Hidden>
+//         <Hidden smDown implementation="css">
+//           <Drawer
+//             variant="permanent"
+//             open
+//             classes={{
+//               paper: classes.drawerPaper,
+//             }}
+//           >
+//             {drawer}
+//           </Drawer>
+//         </Hidden>
+//         <main className={classes.content}>
+//           <div className={classes.toolbar} />
+//           <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+//         </main>
+//       </div>
+//     )
+//   }
+// }
 
 // ResponsiveDrawer.propTypes = {
 //   classes: PropTypes.object.isRequired,
