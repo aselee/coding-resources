0f4a96feca94123a7edda42666436fadf5558771
Andrew <32229265+aselee@users.noreply.github.com>
2018-12-17 18:32:39 -0600

    initial

--- client/components/Resources/ResourceListItem.jsx
+++ client/components/Resources/ResourceListItem.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Button, Chip } from '@material-ui/core';
+import { Grid, Paper, Button, Chip } from '@material-ui/core';
 import { ThumbUp } from '@material-ui/icons';
 
-import './Resource.scss';
+import './ResourceListItem.scss';
 
 const ResourceListItem = ({ id, url, title, tags, votes }) => (
-  <>
-    <h3>
-      <Link to={`/resources/${id}`}>{title}</Link>
-    </h3>
-    <div>
-      <a href={`https://${url}`} target="_blank" rel="noopener noreferrer">
-        {url}
-      </a>
-    </div>
-    <div>
-      {tags.map(tag => (
-        <Chip
-          key={tag}
-          className="tag"
-          label={tag}
-        />
-      ))}
-    </div>
-    <Button className="linkVotes" variant="contained">
-      Upvote: <ThumbUp style={{ margin: '0 10px' }} />
-      {votes}
-    </Button>
-  </>
+  <Grid>
+    <Paper className="main">
+      <div>
+        <h2>
+          <Link to={`/resources/${id}`} className="site">
+            {title}
+          </Link>
+        </h2>
+      </div>
+      <div>
+        test2
+        <a href={`https://${url}`} target="_blank" rel="noopener noreferrer">
+          {url}
+        </a>
+      </div>
+      <div>
+        test3
+        {tags.map(tag => (
+          <Chip
+            key={tag}
+            className="tag"
+            label={tag}
+          />
+        ))}
+        test4
+      </div>
+      <Button className="linkVotes" variant="contained">
+        Upvote: <ThumbUp style={{ margin: '0 10px' }} />
+        {votes}
+      </Button>
+    </Paper>
+  </Grid>
 );
 
 ResourceListItem.propTypes = {
